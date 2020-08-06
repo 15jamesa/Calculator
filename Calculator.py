ba3c719962c074a37940f2a0021f18778184be01
15jamesa <avaraver.aj@gmail.com>
2020-08-06 11:50:56 +0100

    Second commit

--- Calculator.py
+++ Calculator.py
@@ -4,13 +4,22 @@ numberstore = ""
 
 # Result of button pressed
 def press(btn):
-    calc.setLabel("Screen", btn)
-    press.button = int(btn)
-    calc.setLabel("Screen", press.button)
-    global numberstore
-    numberstore = int(str(numberstore) + str(press.button))
-    press.button = numberstore
-    calc.setLabel("Screen", press.button)
+    btn = float(btn)
+    if (btn - int(btn)) == 0:
+        calc.setLabel("Screen", int(btn))
+        press.button = int(btn)
+        calc.setLabel("Screen", int(press.button))
+        global numberstore
+        numberstore = int(str(numberstore) + str(press.button))
+        press.button = numberstore
+        calc.setLabel("Screen", press.button)
+    else:
+        calc.setLabel("Screen", float(btn))
+        press.button = float(btn)
+        calc.setLabel("Screen", float(press.button))
+        numberstore = float(str(numberstore) + str(press.button))
+        press.button = numberstore
+        calc.setLabel("Screen", press.button)
 
 # Result of operation button being pressed
 def op(sign):
@@ -22,7 +31,6 @@ def op(sign):
         num1 = press.button
         store1(num1)
 
-
 # Result of equals being pressed
 def pressLast(equal):
     try:
@@ -30,7 +38,6 @@ def pressLast(equal):
     except AttributeError:
         calc.setLabel("Screen", "Error: Please enter a valid operation \nSelect 'Clear' to continue")
         calc.setFg("Red", "Screen")
-    #operation = op.operation
     num2 = int(press.button)
     if op.operation == "x^2":
         op.result = num2 * num2
@@ -38,23 +45,24 @@ def pressLast(equal):
         global numberstore
         numberstore = ""
         if op.operation == "+":
-            num1 = int(store1.num1)
-            op.result = num1 + num2
+            num1 = float(store1.num1)
+            op.result = float(num1 + num2)
         elif op.operation == "-":
-            num1 = int(store1.num1)
-            op.result = num1 - num2
+            num1 = float(store1.num1)
+            op.result = float(num1 - num2)
         elif op.operation == "X":
-            num1 = int(store1.num1)
-            op.result = num1 * num2
+            num1 = float(store1.num1)
+            op.result = float(num1 * num2)
+        elif op.operation == "/":
+            num1 = float(store1.num1)
+            op.result = float(num1 / num2)
         else:
             pass
-    press(op.result)
-
+    press(float(op.result))
 
 # Store of button presses
 def store1(num1):
-    store1.num1 = num1
-
+    store1.num1 = float(num1)
 
 # Result of "clear" being pressed
 def clear(btn):
@@ -69,7 +77,6 @@ def clear(btn):
     del op.operation
     del op.result
 
-
 # Calculator Layout
 calc = gui("Calculator", "300x450")
 calc.setStretch("both")
@@ -80,22 +87,23 @@ calc.addLabel("Screen", "", 0, 0, 4, 1)
 calc.setLabelBg("Screen", "white")
 
 # Calculator Buttons
-calc.addButton("1", press, 1, 0)
-calc.addButton("2", press, 1, 1)
-calc.addButton("3", press, 1, 2)
-calc.addButton("4", press, 2, 0)
-calc.addButton("5", press, 2, 1)
-calc.addButton("6", press, 2, 2)
-calc.addButton("7", press, 3, 0)
-calc.addButton("8", press, 3, 1)
-calc.addButton("9", press, 3, 2)
-calc.addButton("0", press, 4, 1)
+calc.addButton("1", press, 2, 0)
+calc.addButton("2", press, 2, 1)
+calc.addButton("3", press, 2, 2)
+calc.addButton("4", press, 3, 0)
+calc.addButton("5", press, 3, 1)
+calc.addButton("6", press, 3, 2)
+calc.addButton("7", press, 4, 0)
+calc.addButton("8", press, 4, 1)
+calc.addButton("9", press, 4, 2)
+calc.addButton("0", press, 5, 1)
 calc.addButton("+", op, 1, 3)
 calc.addButton("-", op, 2, 3)
-calc.addButton("x^2", op, 3, 3)
+calc.addButton("/", op, 3,3)
+calc.addButton("x^2", op, 5, 3)
 calc.addButton("X", op, 4, 3)
-calc.addButton("=", pressLast, 5, 0, 4)
-calc.addButton("Clear", clear, 6, 0, 4)
+calc.addButton("Clear", clear, 1, 0, 3)
+calc.addButton("=", pressLast, 6, 0, 4)
 
 # Button Colours
 calc.setButtonBg("0", "PaleTurquoise")
@@ -111,6 +119,7 @@ calc.setButtonBg("9", "PaleTurquoise")
 calc.setButtonBg("+", "LightSkyBlue")
 calc.setButtonBg("-", "LightSkyBlue")
 calc.setButtonBg("x^2", "LightSkyBlue")
+calc.setButtonBg("/", "LightSkyBlue")
 calc.setButtonBg("X", "LightSkyBlue")
 calc.setButtonBg("=", "CornflowerBlue")
 calc.setButtonBg("Clear", "LightBlue")
